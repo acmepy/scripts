aba2b35f6118ed93af0fe8155ef54519ef1a79e3
acmepy <hjgamarra@gmail.com>
2025-08-11 10:36:57 -0300

    add rollback-force

---
+++ oracle/rollback-force.bat
@@ -0,0 +1,20 @@
+@echo off
+setlocal
+
+for /f "tokens=*" %%a in (sucursales.txt) do (
+  for /f "tokens=2 delims=@" %%b in ("%%a") do (
+    echo Procesando %%b
+  )
+  del %tmp%\exe.sql
+  del %tmp%\rollback.sql
+  @(
+    echo set linesize 1000
+    echo spool %tmp%\rollback.sql
+    echo select 'rollback force '''^|^|local_tran_id^|^|''';' from dba_2pc_pending where state = 'prepared';
+    echo spool off;
+    echo exit;
+  )> %tmp%\exe.sql
+ sqlplus %%a @%tmp%\exe.sql
+ echo exit; >> %tmp%\rollback.sql
+ sqlplus %%a @%tmp%\rollback.sql
+)
